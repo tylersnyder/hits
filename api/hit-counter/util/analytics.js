f3cb8ac03307b5b5bf954bfcb2e812a211d1721b
Tyler <tylersnyder@gmail.com>
2019-10-02 22:41:20 -0400

    Update analytics.js

--- api/hit-counter/util/analytics.js
+++ api/hit-counter/util/analytics.js
@@ -1,6 +1,6 @@
 module.exports = async (ctx, next) => {
   const headers = ctx.request.headers;
-  const doc = ctx.db.collection("analytics").doc(`${ctx.referer}`);
+  const doc = ctx.db.collection("analytics").doc(ctx.referer);
 
   // @todo push meta data about each hit to a collection
   doc.set({
